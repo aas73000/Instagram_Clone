8957a4816bc5ac7f9f8671bd16826f14cee4c965
aas73000 <aas73000@gmail.com>
2019-01-02 16:39:02 +0530

    login and signup completed with all bugs fixed

--- app/src/main/java/com/example/administrator/instagram_clone/MainActivity.java
+++ app/src/main/java/com/example/administrator/instagram_clone/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.administrator.instagram_clone;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import com.parse.ParseException;
 import com.parse.ParseInstallation;
@@ -13,16 +18,26 @@ import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button buttonLogIn,buttonSignUp;
     private EditText txtEmail,txtUserName,txtPassword;
+    private ProgressBar progressBar;
+    private CharSequence title = "Sign Up";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+ //       getActionBar().setTitle(title);
+        setActionBarTitle();
         ParseInstallation.getCurrentInstallation().saveInBackground();
         initializeAllViews();
         buttonClicked();
+    }
+    public void setActionBarTitle(){
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(title);
     }
     private void initializeAllViews(){
         buttonLogIn = findViewById(R.id.loginId);
@@ -30,19 +45,41 @@ public class MainActivity extends AppCompatActivity {
         txtEmail = findViewById(R.id.emailId);
         txtUserName = findViewById(R.id.usernameId);
         txtPassword = findViewById(R.id.passwordId);
+        progressBar = findViewById(R.id.indeterminateBar);
     }
     private void buttonClicked(){
-        buttonLogIn.setOnClickListener(new View.OnClickListener() {
+        buttonLogIn.setOnClickListener(MainActivity.this);
+        buttonSignUp.setOnClickListener(MainActivity.this);
+
+
+        txtPassword.setOnKeyListener(new View.OnKeyListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
-                startActivity(intent);
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN){
+                    onClick(buttonSignUp);
+                    return true;
+                }
+                return false;
             }
         });
-        buttonSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.loginId:
+                Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.signupId:
+                progressBar.setAlpha(1);
                 final ParseUser parseUser = new ParseUser();
+                if(txtEmail.getText().toString().equals("") || txtUserName.getText().toString().equals("") || txtPassword.getText().toString().equals("")){
+                    FancyToast.makeText(MainActivity.this,"Fill all the entries",
+                            FancyToast.LENGTH_LONG,FancyToast.INFO,true).show();;
+                    progressBar.setAlpha(0);
+                    return;
+                }
                 parseUser.setEmail(txtEmail.getText().toString());
                 parseUser.setUsername(txtUserName.getText().toString());
                 parseUser.setPassword(txtPassword.getText().toString());
@@ -54,11 +91,21 @@ public class MainActivity extends AppCompatActivity {
                                     FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
                         }else{
                             FancyToast.makeText(MainActivity.this,e.getMessage(),
-                                    FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();;
+                                    FancyToast.LENGTH_LONG,FancyToast.ERROR,true).show();;
                         }
+                        progressBar.setAlpha(0);
                     }
                 });
-            }
-        });
+                break;
+
+        }
+    }
+    public void onLayoutTapped(View view){
+        try{
+        InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);}
+        catch (Exception e){
+            e.getStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/administrator/instagram_clone/SignUpActivity.java
+++ app/src/main/java/com/example/administrator/instagram_clone/SignUpActivity.java
@@ -1,12 +1,17 @@
 package com.example.administrator.instagram_clone;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.parse.LogInCallback;
@@ -14,47 +19,83 @@ import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
-public class SignUpActivity extends AppCompatActivity {
-    private Button loginButton,signupButton;
-    private EditText txtUserName,txtPassword;
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button loginButton, signupButton;
+    private EditText txtUserName, txtPassword;
+    private CharSequence title = "Log In";
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(title);
         initializeAllView();
         buttonClicked();
     }
-    private void initializeAllView(){
+
+    private void initializeAllView() {
         loginButton = findViewById(R.id.loginId2);
         signupButton = findViewById(R.id.signupId2);
         txtUserName = findViewById(R.id.usernameId2);
         txtPassword = findViewById(R.id.passwordId2);
+        progressBar = findViewById(R.id.indeterminateBar2);
     }
-    private void buttonClicked(){
-        signupButton.setOnClickListener(new View.OnClickListener() {
+
+    private void buttonClicked() {
+        signupButton.setOnClickListener(SignUpActivity.this);
+        loginButton.setOnClickListener(SignUpActivity.this);
+        txtPassword.setOnKeyListener(new View.OnKeyListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SignUpActivity.this,MainActivity.class);
-                startActivity(intent);
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    onClick(loginButton);
+                    return true;
+
+                }
+                return false;
             }
         });
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.loginId2:
+                progressBar.setAlpha(1);
                 ParseUser.logInInBackground(txtUserName.getText().toString(), txtPassword.getText().toString(), new LogInCallback() {
                     @Override
                     public void done(ParseUser user, ParseException e) {
-                        Log.i("Working","Function");
-                        if(user != null && e == null){
-                            FancyToast.makeText(SignUpActivity.this,"Login successfully",FancyToast.LENGTH_LONG,
-                                    FancyToast.SUCCESS,true).show();
-                        }else {
-                            FancyToast.makeText(SignUpActivity.this,"error login",FancyToast.LENGTH_LONG,
-                                    FancyToast.ERROR,true).show();
+                        Log.i("Working", "Function");
+                        if (user != null && e == null) {
+                            FancyToast.makeText(SignUpActivity.this, "Login successfully", FancyToast.LENGTH_LONG,
+                                    FancyToast.SUCCESS, true).show();
+                        } else {
+                            FancyToast.makeText(SignUpActivity.this, "error login", FancyToast.LENGTH_LONG,
+                                    FancyToast.ERROR, true).show();
+
                         }
+                        progressBar.setAlpha(0);
+                        ParseUser.logOut();
                     }
                 });
-            }
-        });
+                break;
+            case R.id.signupId2:
+                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+    public void onLayoutTapped(View view){
+        try{
+        InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);}
+        catch (Exception e){
+            e.getStackTrace();
+        }
     }
 }
